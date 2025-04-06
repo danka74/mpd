f7b66ae757f2ac11f34de8ae48f36fd745003ced
Daniel Karlsson <daniel.e.karlsson@gmail.com>
2025-04-06 11:27:07 +0200

    attempt to fix concept map referring to SDs error

--- r4-r5-scripts/prepare-r5.sh
+++ r4-r5-scripts/prepare-r5.sh
@@ -1,5 +1,6 @@
 rm -rf ../input/fsh/diff-fhir-rel
 cp -rf ../input/diff/r5 ../input/fsh/diff-fhir-rel
 cp sushi-config-r5.yaml ../sushi-config.yaml
+cp ../input/diff/r5/input-maps/*.json ../input/resources/
 cp ig-r5.ini ../ig.ini
 # copy alias-ig.r5 ../input/fsh/alias-ig.fsh
